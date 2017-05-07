38320b0c6b65d19fc16f857647f352f6859d8c54
Kevan <kevan@bufferapp.com>
2017-05-06 21:06:29 -0600

    trying something

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
 
 </head>
-
+:)
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'pancake' ); ?></a>
